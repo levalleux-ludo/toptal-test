77b599cc9affdeda56fce6ca68c7227c36108089
Ludovic Levalleux <levalleux_ludo@hotmail.com>
2021-01-22 12:50:14 +0000

    complete Ownership tests for both contracts

--- contracts/test/badium-test.js
+++ contracts/test/badium-test.js
@@ -13,24 +13,32 @@ const tokenSymbol = 'BAD';
 const decimals = 18;
 const initialSupply = 10000000;
 
+async function initTestVariables() {
+    [deployer, account1, account2] = await ethers.getSigners();
+    deployerAddr = await deployer.getAddress();
+    account1Addr = await account1.getAddress();
+    account2Addr = await account2.getAddress();
+    BadiumFactory = await ethers.getContractFactory("Badium");
+    initialOwner = account1Addr;
+}
+
+async function createContract() {
+    badium = await BadiumFactory.deploy(
+        tokenName,
+        tokenSymbol,
+        decimals,
+        hre.ethers.BigNumber.from(initialSupply).mul(hre.ethers.BigNumber.from(10).pow(decimals)),
+        initialOwner);
+
+    await badium.deployed();
+}
+
 describe("Badium Token Deployment", async() => {
     before('', async() => {
-        [deployer, account1, account2] = await ethers.getSigners();
-        deployerAddr = await deployer.getAddress();
-        account1Addr = await account1.getAddress();
-        account2Addr = await account2.getAddress();
-        BadiumFactory = await ethers.getContractFactory("Badium");
-        initialOwner = account1Addr;
+        await initTestVariables();
     })
     it("Should verify that the token contract is deployed", async() => {
-        badium = await BadiumFactory.deploy(
-            tokenName,
-            tokenSymbol,
-            decimals,
-            hre.ethers.BigNumber.from(initialSupply).mul(hre.ethers.BigNumber.from(10).pow(decimals)),
-            initialOwner);
-
-        await badium.deployed();
+        await createContract();
         expect(badium.address).to.not.equal('0x' + '0'.repeat(32));
     });
     it('Verify name', async() => {
@@ -51,22 +59,9 @@ describe("Badium Token Deployment", async() => {
 });
 
 describe('Test Badium token standard ERC20 features', async() => {
-    let balance1Before, balance2Before, balanceOwnerBefore, totalSupplyBefore;
     before('', async() => {
-        [deployer, account1, account2] = await ethers.getSigners();
-        deployerAddr = await deployer.getAddress();
-        account1Addr = await account1.getAddress();
-        account2Addr = await account2.getAddress();
-        BadiumFactory = await ethers.getContractFactory("Badium");
-        initialOwner = account1Addr;
-        badium = await BadiumFactory.deploy(
-            tokenName,
-            tokenSymbol,
-            decimals,
-            hre.ethers.BigNumber.from(initialSupply).mul(hre.ethers.BigNumber.from(10).pow(decimals)),
-            initialOwner);
-
-        await badium.deployed();
+        await initTestVariables();
+        await createContract();
     });
     beforeEach('update balances', async() => {
         balance1Before = await badium.balanceOf(account1Addr);
@@ -98,4 +93,21 @@ describe('Test Badium token standard ERC20 features', async() => {
         expect((await badium.allowance(account1Addr, deployerAddr)).toNumber()).to.equal(0);
     });
 
+});
+
+describe('Badium Ownership', async() => {
+    before('', async() => {
+        await initTestVariables();
+        await createContract();
+    });
+    it('Verify initial owner', async() => {
+        expect(await badium.owner()).to.equal(deployerAddr);
+    });
+    it('The owner can transfer the contract ownership to another account', async() => {
+        await badium.connect(deployer).transferOwnership(account1Addr);
+        expect(await badium.owner()).to.equal(account1Addr);
+    });
+    it('Another user cannot transfer the contract ownership to another account', async() => {
+        await expect(badium.connect(deployer).transferOwnership(account2Addr)).to.be.revertedWith(revertMessage('Ownable: caller is not the owner'));
+    });
 });
--- contracts/test/market-test.js
+++ contracts/test/market-test.js
@@ -7,34 +7,42 @@ let deployer, accoun1, account2;
 let deployerAddr, account1Addr, account2Addr;
 let MarketFactory;
 let market;
+async function initTestVariables() {
+    [deployer, account1, account2] = await ethers.getSigners();
+    deployerAddr = await deployer.getAddress();
+    account1Addr = await account1.getAddress();
+    account2Addr = await account2.getAddress();
+    MarketFactory = await ethers.getContractFactory("Market");
+}
+async function createContract() {
+    market = await MarketFactory.deploy();
+
+    await market.deployed();
+}
 
 describe("Market Deployment", async() => {
     before('', async() => {
-        [deployer, account1, account2] = await ethers.getSigners();
-        deployerAddr = await deployer.getAddress();
-        account1Addr = await account1.getAddress();
-        account2Addr = await account2.getAddress();
-        MarketFactory = await ethers.getContractFactory("Market");
+        await initTestVariables();
     })
     it("Should verify that the Market contract is deployed", async() => {
-        market = await MarketFactory.deploy();
-
-        await market.deployed();
+        await createContract();
         expect(market.address).to.not.equal('0x' + '0'.repeat(32));
     });
 });
 
 describe('Market Ownership', async() => {
     before('', async() => {
-        [deployer, account1, account2] = await ethers.getSigners();
-        deployerAddr = await deployer.getAddress();
-        account1Addr = await account1.getAddress();
-        account2Addr = await account2.getAddress();
-        MarketFactory = await ethers.getContractFactory("Market");
-        market = await MarketFactory.deploy();
-        await market.deployed();
+        await initTestVariables();
+        await createContract();
     });
     it('Verify initial owner', async() => {
         expect(await market.owner()).to.equal(deployerAddr);
+    });
+    it('The owner can transfer the contract ownership to another account', async() => {
+        await market.connect(deployer).transferOwnership(account1Addr);
+        expect(await market.owner()).to.equal(account1Addr);
+    });
+    it('Another user cannot transfer the contract ownership to another account', async() => {
+        await expect(market.connect(deployer).transferOwnership(account2Addr)).to.be.revertedWith(revertMessage('Ownable: caller is not the owner'));
     });
 });
